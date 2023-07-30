5f9e380d43dbefade4e44e4c32e1b70d25b074d4
Being Of Existence <manfromearth25@gmail.com>
2023-07-30 12:24:31 +0000

    feat: never give up

--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -11,46 +11,14 @@ import {
   PageHeaderDescription,
   PageHeaderHeading,
 } from "@/components/page-header"
+import Todo from "@/components/todo"
 import { buttonVariants } from "@/registry/new-york/ui/button"
 import { Separator } from "@/registry/new-york/ui/separator"
 import DashboardPage from "@/app/examples/dashboard/page"
-import Todo from "@/components/todo"
 
 export default function IndexPage() {
   return (
     <div className="container relative">
-      {/* <PageHeader className="pb-8">
-        <Link
-          href="/docs/changelog"
-          className="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium"
-        >
-          🎉 <Separator className="mx-2 h-4" orientation="vertical" />{" "}
-          <span className="sm:hidden">Style, a new CLI and more.</span>
-          <span className="hidden sm:inline">
-            Introducing Style, a new CLI and more.
-          </span>
-          <ArrowRightIcon className="ml-1 h-4 w-4" />
-        </Link>
-        <PageHeaderHeading>Build your component library.</PageHeaderHeading>
-        <PageHeaderDescription>
-          Beautifully designed components that you can copy and paste into your
-          apps. Accessible. Customizable. Open Source.
-        </PageHeaderDescription>
-        <div className="flex w-full items-center space-x-4 pb-8 pt-4 md:pb-10">
-          <Link href="/docs" className={cn(buttonVariants())}>
-            Get Started
-          </Link>
-          <Link
-            target="_blank"
-            rel="noreferrer"
-            href={siteConfig.links.github}
-            className={cn(buttonVariants({ variant: "outline" }))}
-          >
-            <Icons.gitHub className="mr-2 h-4 w-4" />
-            GitHub
-          </Link>
-        </div>
-      </PageHeader> */}
       <PageHeader className=" ">
         <Link
           href="/docs/changelog"
@@ -65,10 +33,12 @@ export default function IndexPage() {
           </span>
           <ArrowRightIcon className="ml-1 h-4 w-4" />
         </Link>
-        <PageHeaderHeading>Build your dreamed component library.</PageHeaderHeading>
+        <PageHeaderHeading>
+          Build your dreamed component library.
+        </PageHeaderHeading>
         <PageHeaderDescription>
-          Beautifully designed components and developer resources that you can use into your
-          Websites. Accessible. Customizable. Open Source.
+          Beautifully designed components and developer resources that you can
+          use into your Websites. Accessible. Customizable. Open Source.
         </PageHeaderDescription>
         <div className="flex w-full items-center space-x-4 pb-8 pt-4 md:pb-10">
           <Link href="/docs" className={cn(buttonVariants())}>
@@ -87,8 +57,8 @@ export default function IndexPage() {
       </PageHeader>
 
       <Todo />
-      
-      <ExamplesNav className="[&>a:first-child]:text-primary" />
+
+      {/* <ExamplesNav className="[&>a:first-child]:text-primary" />
       <section className="space-y-8 overflow-hidden rounded-lg border-2 border-primary dark:border-muted md:hidden">
         <Image
           src="/examples/dashboard-light.png"
@@ -109,7 +79,7 @@ export default function IndexPage() {
         <div className="overflow-hidden rounded-lg border bg-background shadow">
           <DashboardPage />
         </div>
-      </section>
+      </section> */}
     </div>
   )
 }
