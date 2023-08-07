dd4735cd2632bc4cd7ec7314840ee0ecd438b624
Being Of Existence <manfromearth25@gmail.com>
2023-08-07 09:59:18 +0000

    feat: menu icon changed

--- apps/www/components/icons.tsx
+++ apps/www/components/icons.tsx
@@ -989,18 +989,16 @@ export const Icons = {
   menu: (props: IconProps) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
-      viewBox="0 0 24 24"
       fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
     >
-      <line x1="4" x2="20" y1="12" y2="12" />
-      <line x1="4" x2="20" y1="6" y2="6" />
-      <line x1="4" x2="20" y1="18" y2="18" />
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3.75 9h16.5m-16.5 6.75h16.5"
+      />
     </svg>
   ),
   inbox: (props: IconProps) => (
