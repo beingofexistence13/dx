028db6be2e1a79eef44042d22e94013df5bb1e13
Being Of Existence <manfromearth25@gmail.com>
2023-08-11 05:20:37 +0000

    feat: fiesflow is not maded for typescript yet

--- packages/filesflow/src/index.ts
+++ packages/filesflow/src/index.ts
@@ -27,6 +27,26 @@
 // }
 
 // main()
+import argparse from 'argparse';
+import chalk from 'chalk';
+import chalkAnimation from 'chalk-animation';
+import cliui from 'cliui';
+import figlet from 'figlet';
+import gradientString from 'gradient-string';
+import hasFlag from 'has-flag';
+import inquirer from 'inquirer';
+import logSymbols from 'log-symbols';
+import meow from 'meow';
+import nanospinner from 'nanospinner';
+import ora from 'ora';
+import prompts from 'prompts';
+import { Command } from 'commander';
+import stringWidth from 'string-width';
+
+
+chalkAnimation.rainbow('Make Bulk Files by using any kind data,json,csv or any kind or datastorage');
+
+
 
 
 
