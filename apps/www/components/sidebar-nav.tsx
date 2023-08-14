80b781850c8a48219cc90660b6fa83481f4c41d4
Being Of Existence <manfromearth25@gmail.com>
2023-08-14 06:42:18 +0000

    feat: very very important commit docs config is changed

--- apps/www/components/sidebar-nav.tsx
+++ apps/www/components/sidebar-nav.tsx
@@ -14,7 +14,7 @@ export function DocsSidebarNav({ items }: DocsSidebarNavProps) {
   const pathname = usePathname()
 
   return items.length ? (
-    <div className="w-full mt-3 ">
+    <div className="mt-5 w-full ">
       {items.map((item, index) => (
         <div key={index} className={cn("pb-4")}>
           <h4 className="mb-1 rounded-md px-2 py-1 text-sm font-semibold">
