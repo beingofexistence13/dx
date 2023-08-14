3130b1d7bb4b34fb2c293659aea43103a5419e45
Being Of Existence <manfromearth25@gmail.com>
2023-08-14 11:38:02 +0000

    feat: nextjs app router

--- apps/www/app/ui/page.tsx
+++ apps/www/app/ui/page.tsx
@@ -20,40 +20,12 @@ export default function IndexPage() {
   return (
     <div className="container relative">
       <PageHeader className=" ">
-        <Link
-          href="/docs/changelog"
-          className="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium"
-        >
-          🎉 <Separator className="mx-2 h-4" orientation="vertical" />{" "}
-          <span className="sm:hidden">
-            Make your Web Devlopmemt flawless with beingofexistence/dx
-          </span>
-          <span className="hidden sm:inline">
-           Ui
-          </span>
-          <ArrowRightIcon className="ml-1 h-4 w-4" />
-        </Link>
+
         <PageHeaderHeading>
-          Build your dreamed component library.
+          Ui
         </PageHeaderHeading>
-        <PageHeaderDescription>
-          Beautifully designed components and developer resources that you can
-          use into your Websites. Accessible. Customizable. Open Source.
-        </PageHeaderDescription>
-        <div className="flex w-full items-center space-x-4 pb-8 pt-4 md:pb-10">
-          <Link href="/docs" className={cn(buttonVariants())}>
-            Get Started
-          </Link>
-          <Link
-            target="_blank"
-            rel="noreferrer"
-            href={siteConfig.links.github}
-            className={cn(buttonVariants({ variant: "outline" }))}
-          >
-            <Icons.gitHub className="mr-2 h-4 w-4" />
-            GitHub
-          </Link>
-        </div>
+
+
       </PageHeader>
     </div>
   )
