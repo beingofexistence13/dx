4c1d5041ba1245525ecb74d8839650b0938b5f41
Being Of Existence <manfromearth25@gmail.com>
2023-08-21 14:28:51 +0000

    feat: hello

--- apps/www/app/docs/[[...slug]]/not-found.tsx
+++ apps/www/app/docs/[[...slug]]/not-found.tsx
@@ -1,13 +1,20 @@
-import { Boundary } from '@/ui/boundary';
+import { Boundary } from "@/ui/boundary"
 
 export default function NotFound() {
   return (
-    <Boundary labels={['not-found.tsx']} color="pink">
-      <div className="text-vercel-pink space-y-4">
-        <h2 className="text-lg font-bold">Not Found</h2>
-
-        <p className="text-sm">Could not find requested resource</p>
-      </div>
-    </Boundary>
-  );
+    <div>
+      {/* <h2 className="text-lg font-bold">
+        This Page Is In Devlopment And Will Be Added Soon!!!
+      </h2> */}
+      <h2 className="text-lg font-bold">
+        This page  is in devlopment(coming soon!!!)
+      </h2>
+    </div>
+  )
 }
+// <Boundary labels={['not-found.tsx']} color="pink">
+//   <div className="text-vercel-pink space-y-4">
+//     <h2 className="text-lg font-bold">Not Found</h2>
+//     <p className="text-sm">Could not find requested resource</p>
+//   </div>
+// </Boundary>
