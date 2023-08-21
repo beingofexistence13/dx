fe05f1b99615468d33e8cf048a80f18d607b4fb7
Being Of Existence <manfromearth25@gmail.com>
2023-08-21 21:12:26 +0000

    feat: not found page updated

--- apps/www/app/docs/[[...slug]]/not-found.tsx
+++ apps/www/app/docs/[[...slug]]/not-found.tsx
@@ -1,20 +1,106 @@
-import { Boundary } from "@/ui/boundary"
+import { notFound } from "next/navigation"
+import { allDocs } from "contentlayer/generated"
 
-export default function NotFound() {
+import "@/styles/mdx.css"
+import type { Metadata } from "next"
+import Link from "next/link"
+import { ChevronRightIcon } from "@radix-ui/react-icons"
+import Balancer from "react-wrap-balancer"
+
+import { siteConfig } from "@/config/site"
+import { getTableOfContents } from "@/lib/toc"
+import { absoluteUrl, cn } from "@/lib/utils"
+import { Icons } from "@/components/icons"
+import { Mdx } from "@/components/mdx-components"
+import { DocsPager } from "@/components/pager"
+import { DashboardTableOfContents } from "@/components/toc"
+import { badgeVariants } from "@/registry/new-york/ui/badge"
+import { ScrollArea } from "@/registry/new-york/ui/scroll-area"
+
+interface DocPageProps {
+  params: {
+    slug: string[]
+  }
+}
+
+async function getDocFromParams({ params }: DocPageProps) {
+  const doc = params.slug?.join("/") || ""
+  // const doc = allDocs.find((doc) => doc.slugAsParams === slug)
+
+  if (!doc) {
+    null
+  }
+
+  return doc
+}
+
+export async function generateMetadata({
+  params,
+}: DocPageProps): Promise<Metadata> {
+  const doc = await getDocFromParams({ params })
+
+  if (!doc) {
+    return {}
+  }
+// doc is now slug
+  return {
+    title: "Not Found(coming soon!!!)",
+    description:  "Not Found(coming soon!!!)",
+    openGraph: {
+      title:  "Not Found(coming soon!!!)",
+      description:  "Not Found(coming soon!!!)",
+      type: "article",
+      url: absoluteUrl(doc),
+      images: [
+        {
+          url: siteConfig.ogImage,
+          width: 1200,
+          height: 630,
+          alt: siteConfig.name,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title:  "Not Found(coming soon!!!)",
+      description: "Not Found(coming soon!!!)",
+      images: [siteConfig.ogImage],
+      creator: "@beingofexistence",
+    },
+  }
+}
+
+export async function generateStaticParams(): Promise<
+  DocPageProps["params"][]
+> {
+  return allDocs.map((doc) => ({
+    slug: doc.slugAsParams.split("/"),
+  }))
+}
+
+// async function DocPage({ params }: DocPageProps) {
+//   const doc = await getDocFromParams({ params })
+
+//   if (!doc) {
+//     notFound()
+//   }
+
+// }
+
+export default async function NotFound({ params }: DocPageProps) {
+
+  const doc = await getDocFromParams({ params })
+
+  if (!doc) {
+    notFound()
+  }
+
   return (
-    <div>
-      {/* <h2 className="text-lg font-bold">
-        This Page Is In Devlopment And Will Be Added Soon!!!
-      </h2> */}
+    <div className="flex h-full w-full items-center justify-center">
       <h2 className="text-lg font-bold">
-        This page is in devlopment(coming soon!!ghfgh!)
+        {`This ${doc} page in devlopment(coming soon!!ghfgh!)`}
       </h2>
     </div>
   )
 }
-// <Boundary labels={['not-found.tsx']} color="pink">
-//   <div className="text-vercel-pink space-y-4">
-//     <h2 className="text-lg font-bold">Not Found</h2>
-//     <p className="text-sm">Could not find requested resource</p>
-//   </div>
-// </Boundary>
+
--- apps/www/components/tailwind-indicator.tsx
+++ apps/www/components/tailwind-indicator.tsx
@@ -2,7 +2,7 @@ export function TailwindIndicator() {
   // if (process.env.NODE_ENV === "production") return null
 
   return (
-    <div className="fixed bottom-16 left-2 z-50 flex h-6 w-6 items-center justify-center rounded-full border p-4 font-mono text-xs xs:bottom-20 sm:bottom-2">
+    <div className="fixed bottom-16 left-2 z-[100000000000000000000] flex h-6 w-6 items-center justify-center rounded-full border p-4 font-mono text-xs xs:bottom-20 sm:bottom-2">
       <div className="hidden 2xs:block">2xs</div>
       <div className="block 2xs:hidden sm:hidden">xs</div>
       <div className="hidden sm:block md:hidden">sm</div>
