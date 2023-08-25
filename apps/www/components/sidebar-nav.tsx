59ae183ba11d07c0a0feffde111cca3b0d7f8713
Being Of Existence <manfromearth25@gmail.com>
2023-08-25 11:43:11 +0000

    feat: oneshoot nextjs image

--- apps/www/components/sidebar-nav.tsx
+++ apps/www/components/sidebar-nav.tsx
@@ -184,16 +184,24 @@ export function DocsSidebarNavItems({
             <HoverCard>
               <HoverCardTrigger className="flex h-[35px] w-full items-center ">
                 <div className="items-logo-container rainbow-text mr-2 h-[32.5px] w-[32.5px] border text-center text-[12.5px] rounded-full flex items-center justify-center ">
-                  <Image
-                    src={item.logo ? `/docs/${item.title.replace(/\s/g,"").toLowerCase()}.png` : ""}
-                    height={25}
-                    width={25}
-                    sizes="(max-width: 30px) 100vw"
-                    quality={100}
-                    style={{ objectFit: "contain" }}
-                    loading="lazy"
-                    alt={item.title ? logoLetter(item.title) : "Dx"}
-                  />
+                  {item.logo ? (
+                    <Image
+                      src={`/docs/${item.title
+                        .replace(/\s/g, "")
+                        .toLowerCase()}.png`}
+                      height={25}
+                      width={25}
+                      sizes="(max-width: 30px) 100vw"
+                      quality={100}
+                      style={{ objectFit: "contain" }}
+                      loading="lazy"
+                      alt={item.title ? logoLetter(item.title) : "Dx"}
+                    />
+                  ) : (
+                    <div className="item-logo-fallback">
+                      {item.title ? logoLetter(item.title) : "Dx"}
+                    </div>
+                  )}
                 </div>
                 {/* <Avatar className="rainbow-text mr-2 h-[32.5px] w-[32.5px] border text-center text-[12.5px]">
                   <AvatarImage
