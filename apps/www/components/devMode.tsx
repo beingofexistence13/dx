f3c1650952e1f3a734058623ddb2e293866fd47f
Being Of Existence <manfromearth25@gmail.com>
2023-08-27 15:41:32 +0000

    feat: not found page rocks

--- apps/www/components/devMode.tsx
+++ apps/www/components/devMode.tsx
@@ -275,7 +275,7 @@ export function DevMode() {
 
   return (
     <Dialog>
-      <DialogTrigger className="devMode border-top fixed bottom-16 right-2 flex h-[50px] w-[50px] flex-row items-center justify-center overflow-hidden rounded-full p-2 xs:bottom-20 sm:bottom-3">
+      <DialogTrigger className="devMode border-top fixed bottom-16 right-5 flex h-[50px] w-[50px] flex-row items-center justify-center overflow-hidden rounded-full p-2 xs:bottom-20 sm:bottom-3">
         <div
           className={cn(
             buttonVariants({
