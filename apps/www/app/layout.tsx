68bad06ee37311610f84d45019b6211da3f03e63
Being Of Existence <manfromearth25@gmail.com>
2023-08-30 16:07:26 +0000

    feat: hackIn is really almost complete

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -87,7 +87,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
                   <SiteLayout />
                   {children}
                 </main>
-                <Script src="./script.js"/>
               </Redux>
             </ThemeProvider>
           </Nextui>
