949729388026a6565873b99e690e1232f67171e3
Being Of Existence <manfromearth25@gmail.com>
2023-09-02 06:15:27 +0000

    chore: chainlist added

--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -11,58 +11,111 @@ import { siteConfig } from "@/config/site"
 import { Button } from "@/components/ui/button"
 import { GithubIcon } from "@/components/icons"
 import { subtitle, title } from "@/components/primitives"
+import { AdBanner } from "../components/AdBanner";
+import Layout from "../components/Layout";
+import Chain from "../components/chain";
+import { generateChainData } from "../utils/fetch";
+import Head from "next/head";
+// import { useRouter } from "next/navigation";
+import { usePathname, useSearchParams } from 'next/navigation'
+import * as React from "react";
+
+
 
 export default function Home() {
+  interface Chain {
+    chain: any;
+    chainId: any;
+    nativeCurrency: any;
+    name: string;
+    title: string;
+    network: string;
+    // ...
+  }
+  const [chains, setChains] = React.useState<Chain[]>([]);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const sortedChains = await generateChainData();
+      setChains(sortedChains);
+    }
+    fetchData();
+  }, []);
+  // const router = useRouter();
+  // const { testnets, testnet, search } = router.query;
+  const pathname = usePathname()
+  const searchParams = useSearchParams();
+  const testnets = searchParams ? searchParams.get("testnets") : "";
+  const testnet = searchParams ? searchParams.get("testnet") : "";
+  const search = searchParams ? searchParams.get("search") : "";
+
+  const includeTestnets =
+    (typeof testnets === "string" && testnets === "true") || (typeof testnet === "string" && testnet === "true");
+
+  const sortedChains = !includeTestnets
+    ? chains.filter((item) => {
+        const testnet =
+          item.name?.toLowerCase().includes("test") ||
+          item.title?.toLowerCase().includes("test") ||
+          item.network?.toLowerCase().includes("test");
+        const devnet =
+          item.name?.toLowerCase().includes("devnet") ||
+          item.title?.toLowerCase().includes("devnet") ||
+          item.network?.toLowerCase().includes("devnet");
+        return !testnet && !devnet;
+      })
+    : chains;
+
+  const filteredChains =
+    !search || typeof search !== "string" || search === ""
+      ? sortedChains
+      : sortedChains.filter((chain) => {
+          //filter
+          return (
+            chain.chain.toLowerCase().includes(search.toLowerCase()) ||
+            chain.chainId.toString().toLowerCase().includes(search.toLowerCase()) ||
+            chain.name.toLowerCase().includes(search.toLowerCase()) ||
+            (chain.nativeCurrency ? chain.nativeCurrency.symbol : "").toLowerCase().includes(search.toLowerCase())
+          );
+        });
+
+
+
+
+
+
+
+
+
+
+
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
         <h1 className={title()}>Make&nbsp;</h1>
         <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
         <br />
-        {/* <h1 className={title()}>
-          websites regardless of your design experience.
-        </h1> */}
-        {/* <h2 className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </h2> */}
-        {/* <Button>Shadcn/ui with Nextui</Button> */}
       </div>
-      {/* <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-        <Input type="email" label="Email" />
-        <Input type="email" label="Email" placeholder="Enter your email" />
-      </div> */}
-      {/* 
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          as={NextLink}
-          href={siteConfig.links.docs}
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          as={NextLink}
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div> */}
 
-      {/* <div className="mt-8">
-        <Snippet hideSymbol hideCopyButton variant="flat">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div> */}
+      <Layout lang={undefined}>
+        <React.Suspense fallback={<div className="h-screen"></div>}>
+          <div className="dark:text-[#B3B3B3] text-black grid gap-5 grid-cols-1 place-content-between pb-4 sm:pb-10 sm:grid-cols-[repeat(auto-fit,_calc(50%_-_15px))] 3xl:grid-cols-[repeat(auto-fit,_calc(33%_-_20px))] isolate grid-flow-dense">
+            {filteredChains.map((chain, idx) => {
+              if (idx === 2) {
+                return (
+                  <React.Fragment key={JSON.stringify(chain) + "en" + "with-banner"}>
+                    <AdBanner />
+                    <Chain chain={chain} lang="en" buttonOnly={undefined} />
+                  </React.Fragment>
+                );
+              }
+
+              return <Chain chain={chain} key={JSON.stringify(chain) + "en"} lang="en" buttonOnly={undefined} />;
+            })}
+          </div>
+        </React.Suspense>
+      </Layout>
+
     </section>
   )
 }
