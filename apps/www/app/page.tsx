fddc58d216c8a6ac851ecf9238d03ebd13201a2b
Being Of Existence <manfromearth25@gmail.com>
2023-09-02 06:16:44 +0000

    feat: chinlist

--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -1,6 +1,10 @@
 "use client"
 
+import * as React from "react"
+import Head from "next/head"
 import NextLink from "next/link"
+// import { useRouter } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation"
 import { Code } from "@nextui-org/code"
 import { Link } from "@nextui-org/link"
 import { Input } from "@nextui-org/react"
@@ -11,60 +15,56 @@ import { siteConfig } from "@/config/site"
 import { Button } from "@/components/ui/button"
 import { GithubIcon } from "@/components/icons"
 import { subtitle, title } from "@/components/primitives"
-import { AdBanner } from "../components/AdBanner";
-import Layout from "../components/Layout";
-import Chain from "../components/chain";
-import { generateChainData } from "../utils/fetch";
-import Head from "next/head";
-// import { useRouter } from "next/navigation";
-import { usePathname, useSearchParams } from 'next/navigation'
-import * as React from "react";
 
-
+import { AdBanner } from "../components/AdBanner"
+import Layout from "../components/Layout"
+import Chain from "../components/chain"
+import { generateChainData } from "../utils/fetch"
 
 export default function Home() {
   interface Chain {
-    chain: any;
-    chainId: any;
-    nativeCurrency: any;
-    name: string;
-    title: string;
-    network: string;
+    chain: any
+    chainId: any
+    nativeCurrency: any
+    name: string
+    title: string
+    network: string
     // ...
   }
-  const [chains, setChains] = React.useState<Chain[]>([]);
+  const [chains, setChains] = React.useState<Chain[]>([])
 
   React.useEffect(() => {
     async function fetchData() {
-      const sortedChains = await generateChainData();
-      setChains(sortedChains);
+      const sortedChains = await generateChainData()
+      setChains(sortedChains)
     }
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
   // const router = useRouter();
   // const { testnets, testnet, search } = router.query;
   const pathname = usePathname()
-  const searchParams = useSearchParams();
-  const testnets = searchParams ? searchParams.get("testnets") : "";
-  const testnet = searchParams ? searchParams.get("testnet") : "";
-  const search = searchParams ? searchParams.get("search") : "";
+  const searchParams = useSearchParams()
+  const testnets = searchParams ? searchParams.get("testnets") : ""
+  const testnet = searchParams ? searchParams.get("testnet") : ""
+  const search = searchParams ? searchParams.get("search") : ""
 
   const includeTestnets =
-    (typeof testnets === "string" && testnets === "true") || (typeof testnet === "string" && testnet === "true");
+    (typeof testnets === "string" && testnets === "true") ||
+    (typeof testnet === "string" && testnet === "true")
 
   const sortedChains = !includeTestnets
     ? chains.filter((item) => {
         const testnet =
           item.name?.toLowerCase().includes("test") ||
           item.title?.toLowerCase().includes("test") ||
-          item.network?.toLowerCase().includes("test");
+          item.network?.toLowerCase().includes("test")
         const devnet =
           item.name?.toLowerCase().includes("devnet") ||
           item.title?.toLowerCase().includes("devnet") ||
-          item.network?.toLowerCase().includes("devnet");
-        return !testnet && !devnet;
+          item.network?.toLowerCase().includes("devnet")
+        return !testnet && !devnet
       })
-    : chains;
+    : chains
 
   const filteredChains =
     !search || typeof search !== "string" || search === ""
@@ -73,21 +73,16 @@ export default function Home() {
           //filter
           return (
             chain.chain.toLowerCase().includes(search.toLowerCase()) ||
-            chain.chainId.toString().toLowerCase().includes(search.toLowerCase()) ||
+            chain.chainId
+              .toString()
+              .toLowerCase()
+              .includes(search.toLowerCase()) ||
             chain.name.toLowerCase().includes(search.toLowerCase()) ||
-            (chain.nativeCurrency ? chain.nativeCurrency.symbol : "").toLowerCase().includes(search.toLowerCase())
-          );
-        });
-
-
-
-
-
-
-
-
-
-
+            (chain.nativeCurrency ? chain.nativeCurrency.symbol : "")
+              .toLowerCase()
+              .includes(search.toLowerCase())
+          )
+        })
 
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
@@ -103,19 +98,27 @@ export default function Home() {
             {filteredChains.map((chain, idx) => {
               if (idx === 2) {
                 return (
-                  <React.Fragment key={JSON.stringify(chain) + "en" + "with-banner"}>
+                  <React.Fragment
+                    key={JSON.stringify(chain) + "en" + "with-banner"}
+                  >
                     <AdBanner />
                     <Chain chain={chain} lang="en" buttonOnly={undefined} />
                   </React.Fragment>
-                );
+                )
               }
 
-              return <Chain chain={chain} key={JSON.stringify(chain) + "en"} lang="en" buttonOnly={undefined} />;
+              return (
+                <Chain
+                  chain={chain}
+                  key={JSON.stringify(chain) + "en"}
+                  lang="en"
+                  buttonOnly={undefined}
+                />
+              )
             })}
           </div>
         </React.Suspense>
       </Layout>
-
     </section>
   )
 }
