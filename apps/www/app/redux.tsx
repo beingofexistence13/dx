896ba518834f8b25c0973598377ffdc2f04340d0
Being Of Existence <manfromearth25@gmail.com>
2023-09-02 07:42:37 +0000

    feat: please

--- apps/www/app/redux.tsx
+++ apps/www/app/redux.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Provider } from "react-redux"
-
+import * as React from "react";
 import { store } from "../hooks/store"
 
 export function Redux({ children }: { children: React.ReactNode }) {
