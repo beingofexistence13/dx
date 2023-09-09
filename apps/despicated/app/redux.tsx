ffd6bde6a16de98c82926b76e05e03c30f6029f3
Being Of Existence <manfromearth25@gmail.com>
2023-09-09 14:33:26 +0000

    feat: www updated now will add some packages

--- apps/despicated/app/redux.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import { Provider } from "react-redux"
-import * as React from "react";
-import { store } from "../hooks/store"
-
-export function Redux({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>
-}
