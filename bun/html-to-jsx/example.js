c1bfda723da3308d75da7e04dd485cf49c3db61f
Being Of Existence <manfromearth25@gmail.com>
2023-09-12 04:04:06 +0000

    feat: on my way to make html to nextjs13 converter

--- bun/html-to-jsx/example.js
+++
@@ -1,7 +0,0 @@
-var convert = require('./index');
-
-var html_str = '<div class="foo" style="color: red;"><label for="input"></label><input id="input" /><span>{this.state.value}</span></div>';
-
-var jsx_str = convert(html_str);
-
-console.log(jsx_str);
