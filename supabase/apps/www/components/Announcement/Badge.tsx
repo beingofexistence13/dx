31c0ba33d804bbd1a4d88aa492d6b9a6493e948e
Being Of Existence <manfromearth25@gmail.com>
2023-09-26 15:53:15 +0000

    feat: hi

--- supabase/apps/www/components/Announcement/Badge.tsx
+++ supabase/apps/www/components/Announcement/Badge.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable tailwindcss/migration-from-tailwind-2 */
 import React from 'react'
-import Link from 'next/link'
 import { Badge } from 'ui'
 import { ArrowNarrowRightIcon } from '@heroicons/react/outline'
+import Link from 'next/link'
+import { PropsWithChildren } from 'react'
 
 interface Props {
   url: string
@@ -9,28 +11,29 @@ interface Props {
   badge?: string
   target?: '_self' | '_blank'
 }
-const AnnouncementBadge = ({ url, announcement, badge, target = '_self' }: Props) => (
-  <div className="w-full max-w-xl flex justify-center opacity-0 !animate-[fadeIn_0.5s_cubic-bezier(0.25,0.25,0,1)_0.5s_both]">
-    <Link href={url}>
+
+const AnnouncementBadge = ({ url, announcement, badge, target = '_self' }: PropsWithChildren<Props>) => (
+  <div className="flex w-full max-w-xl !animate-[fadeIn_0.5s_cubic-bezier(0.25,0.25,0,1)_0.5s_both] justify-center opacity-0">
+    <Link href={url} passHref>
       <a
         target={target}
         className="
-          group
-          relative
-          flex flex-row
-          items-center
-          pr-3 p-1
-          text-sm
-          w-auto
-          gap-2
-          text-left
-          rounded-full
-          bg-opacity-20
-          border
           border-background-surface-100
           hover:border-background-surface-300
+          focus:ring-brand-600 group
+          relative
+          flex w-auto
+          flex-row
+          items-center
+          gap-2
           overflow-hidden
-          focus:outline-none focus:ring-brand-600 focus:ring-2 focus:rounded-full
+          rounded-full
+          border
+          bg-opacity-20
+          p-1
+          pr-3
+          text-left
+          text-sm focus:rounded-full focus:outline-none focus:ring-2
           "
       >
         {badge && (
@@ -39,13 +42,13 @@ const AnnouncementBadge = ({ url, announcement, badge, target = '_self' }: Props
           </Badge>
         )}
         <span className="text-foreground">{announcement}</span>
-        <ArrowNarrowRightIcon className="h-4 ml-2 -translate-x-1 transition-transform group-hover:translate-x-0" />
+        <ArrowNarrowRightIcon className="ml-2 h-4 -translate-x-1 transition-transform group-hover:translate-x-0" />
         <div
-          className="absolute inset-0 -z-10 bg-gradient-to-br
-            opacity-70
+          className="from-background-surface-100 to-background-surface-300 absolute inset-0
+            -z-10
             overflow-hidden rounded-full
-            from-background-surface-100
-            to-background-surface-300
+            bg-gradient-to-br
+            opacity-70
             backdrop-blur-md
             "
         />
@@ -55,3 +58,4 @@ const AnnouncementBadge = ({ url, announcement, badge, target = '_self' }: Props
 )
 
 export default AnnouncementBadge
+
