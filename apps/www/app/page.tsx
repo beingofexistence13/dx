930ac750ee6f633687e30048d30d77aef3a19f5a
Being Of Existence <manfromearth25@gmail.com>
2023-11-05 15:49:04 +0000

    chore: worst things to do is algorithms

---
+++ apps/www/app/page.tsx
@@ -0,0 +1,15 @@
+
+export default function Home() {
+  return (
+    <>
+    {/* Project info */}
+    {/* Goal1 -  */}
+    {/* Goal2 - */}
+    {/* Goal3 - */}
+    {/* Goal3 - */}
+    {/* Goal4 - */}
+    {/* Goal5 - */}
+    {/* Footer */}
+    </>
+  )
+}
