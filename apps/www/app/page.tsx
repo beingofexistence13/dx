824b0eac35c5b24c51bfa7285d7a21cb553135f7
Being Of Existence <manfromearth25@gmail.com>
2023-11-06 09:05:23 +0000

    feat: langing page roadmap is kinda done

--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -3,12 +3,11 @@ export default function Home() {
   return (
     <>
     {/* Project info */}
-    {/* Goal1 -  */}
-    {/* Goal2 - */}
-    {/* Goal3 - */}
-    {/* Goal3 - */}
-    {/* Goal4 - */}
-    {/* Goal5 - */}
+    {/* Goal1 - Mindmap and Roadmap with flowchat */}
+    {/* Goal2 - Ai Beautiful space using gradio */}
+    {/* Goal3 - Manage Your Deploys for vercel and all other hosting platfroms */}
+    {/* Goal4 - Seamingly Engaging Workflows */}
+    {/* Goal5 - All your web tools on same place */}
     {/* Footer */}
     </>
   )
