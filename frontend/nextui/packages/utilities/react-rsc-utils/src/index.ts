58d2b0e7b05cf4628d2bbe1b7585e46709e960ef
Being Of Existence <manfromearth25@gmail.com>
2023-12-07 13:31:48 +0000

    feat: old dx

--- frontend/nextui/packages/utilities/react-rsc-utils/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./children";
-export * from "./filter-dom-props";
-export * from "./dom-props";
-export * from "./functions";
--- frontend/www/app/page.tsx
+++ frontend/www/app/page.tsx
@@ -1,3 +1,4 @@
+import { Image } from "@nextui-org/react";
 
 export default function Home() {
   return (
@@ -5,8 +6,15 @@ export default function Home() {
       <section className="info h-[150vh] w-full bg-white relative">
         <div className="blurry_gradient h-[450px] w-[450px] rounded-full absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%] z-[0]"></div>
         <div className="glass h-full w-full"></div>
+
       </section>
       <section className="more_info h-screen w-full bg-red-500">
+        <Image
+          width={300}
+          height={200}
+          alt="NextUI hero Image with delay"
+          src="https://app.requestly.io/delay/5000/https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+        />
       </section>
     </>
   )
