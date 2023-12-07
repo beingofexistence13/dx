5391bf09771f196da9a413a3dc0802e2242c6dd4
Being Of Existence <manfromearth25@gmail.com>
2023-12-07 13:39:44 +0000

    feat: gradient text

--- frontend/www/app/page.tsx
+++ frontend/www/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <>
       <section className="info h-[150vh] w-full bg-white relative">
+
         <div className="blurry_gradient h-[450px] w-[450px] rounded-full absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%] z-[0]"></div>
         <div className="glass h-full w-full"></div>
 
