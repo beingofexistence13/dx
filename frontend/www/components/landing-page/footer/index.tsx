6ad88098d4c07aaac910ae57589d2c077776893c
Being Of Existence <manfromearth25@gmail.com>
2023-12-11 13:57:08 +0000

    feat: footer is easy but donot feel to do  anything

--- frontend/www/components/landing-page/footer/index.tsx
+++ frontend/www/components/landing-page/footer/index.tsx
@@ -1,13 +1,19 @@
-// import "./footer.css"
+import "./footer.css"
 
 const Footer = () => {
   return (
-    <footer className="footer h-auto w-full">
-        <span className="demo-text">
-            Footer
-        </span>
-    </footer>
+    <section className="footer-container h-auto w-full border-t">
+      <footer id="footer" className="footer mx-auto flex h-auto max-w-[1200px] flex-row items-start justify-center space-x-3 p-3">
+        <ul className="brand">
+          <h1 className="brand-name">DX</h1>
+        </ul>
+        <ul className="product">
+          <h1 className="text-md">Product</h1>
+        </ul>
+
+      </footer>
+    </section>
   )
 }
 
-export default Footer
+export default Footer;
